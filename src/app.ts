36990304775db79913c16210edb56568a2b08374
singcl <1276837689@qq.com>
2023-05-15 17:24:09 +0800

    feat: update

--- src/app.ts
+++ src/app.ts
@@ -14,18 +14,6 @@ class App extends Component<PropsWithChildren> {
     Taro.login().then(() => {
       // 发送 res.code 到后台换取 openId, sessionKey, unionId
     });
-    Taro.getSetting({
-      success: (res) => {
-        if (res.authSetting["scope.userInfo"]) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          Taro.getUserInfo({
-            success: (info) => {
-              console.log("-----userinfo", info);
-            },
-          });
-        }
-      },
-    });
   }
 
   componentDidShow() {}

663a7aeca453acee263da71dcab1c41a805515fa
singcl <1276837689@qq.com>
2023-05-15 16:09:35 +0800

    chore(init): init taro app

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { Component, PropsWithChildren } from 'react'
+import './app.styl'
+
+class App extends Component<PropsWithChildren> {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App

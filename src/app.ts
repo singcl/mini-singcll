7118b2decb568ee4e1bee8ca1facfb3365c5ecec
singcl <1276837689@qq.com>
2023-05-15 17:12:27 +0800

    feat: update

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,41 @@
-import { Component, PropsWithChildren } from 'react'
-import './app.styl'
+import { Component, PropsWithChildren } from "react";
+import Taro from "@tarojs/taro";
+import "./app.styl";
 
 class App extends Component<PropsWithChildren> {
+  componentDidMount() {}
 
-  componentDidMount () {}
+  onLaunch(options) {
+    // 展示本地存储能力
+    var logs = Taro.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    Taro.setStorageSync("logs", logs);
 
-  componentDidShow () {}
+    Taro.login().then(() => {
+      // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    });
+    Taro.getSetting({
+      success: (res) => {
+        if (res.authSetting["scope.userInfo"]) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          Taro.getUserInfo({
+            success: (info) => {
+              console.log("-----userinfo", info);
+            },
+          });
+        }
+      },
+    });
+  }
 
-  componentDidHide () {}
+  componentDidShow() {}
+
+  componentDidHide() {}
 
   // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
+  render() {
+    return this.props.children;
   }
 }
 
-export default App
+export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,29 +1,34 @@
-import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { Component, PropsWithChildren } from "react";
+import { View, Text, OpenData } from "@tarojs/components";
+import { AtButton } from "taro-ui";
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
-import './index.styl'
+import "taro-ui/dist/style/components/button.scss"; // 按需引入
+import "./index.styl";
 
 export default class Index extends Component<PropsWithChildren> {
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='index'>
+      <View className="index">
+        <OpenData type="userNickName"></OpenData>
         <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
+        <AtButton type="primary">I need Taro UI</AtButton>
         <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle>支持</AtButton>
+        <AtButton type="primary" circle>
+          支持
+        </AtButton>
         <Text>共建？</Text>
-        <AtButton type='secondary' circle>来</AtButton>
+        <AtButton type="secondary" circle>
+          来
+        </AtButton>
       </View>
-    )
+    );
   }
 }
